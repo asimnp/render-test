fb16f5c61f030cad17b3ed91a301e2f35f539d5f
asim <asimshrestha608@gmail.com>
2024-08-16 16:43:50 +0545

    connecting the backend to a database

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
+
+const url = process.env.MONGODB_URI;
+
+// mongoose settings
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
 let notes = [
   {
@@ -37,7 +60,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
