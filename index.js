8a80937bbf6b27fbfad9b870031c29a5d154d339
asim <asimshrestha608@gmail.com>
2024-08-18 16:15:10 +0545

    refactor

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const Note = require("./models/note");
 
 const app = express();
 
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(cors());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:", request.path);
+  console.log("Body:", request.body);
+  console.log("---");
+  next();
+};
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(express.static("dist"));
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => [
+  response.status(404).send({ error: "unknown endpoint" }),
 ];
 
 app.get("/", (request, response) => {
@@ -89,22 +95,7 @@ app.delete("/api/notes/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "Unknown endpoint" });
-};
-
 app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  }
-
-  next(error);
-};
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3002;
